dcc5a521c86074367298021195cd1b1e7a232fac
shubham sahoo <56964646+shubham9793@users.noreply.github.com>
2023-01-09 10:08:12 +0530

    Delete Hospital directory

--- Hospital/src/main/java/com/Hospital/Repository/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Hospital.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Hospital.Entity.New.Doctor;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-
-}
--- Hospital/src/main/java/com/Hospital/Repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Hospital.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Hospital.Entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
